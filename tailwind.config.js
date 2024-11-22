a5f2c30ae48887c5aa5fd1cbcfef96ca6886d428
Miguel <miguelagustin182@gmail.com>
2024-11-22 02:06:31 -0600

    Eliminar archivos innecesarios para producción en main

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./public/**/*.html', './src/**/*.js'], // Archivos que contienen clases de Tailwind
-  theme: {
-      extend: {},
-  },
-  plugins: [],
-};
