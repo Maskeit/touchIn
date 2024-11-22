f9dc59ff26906bf4c6e0edff52919563de13341e
Miguel <miguelagustin182@gmail.com>
2024-11-22 02:44:51 -0600

    fix:Actualizar conf de mysql en config

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', '127.0.0.1');  // O 'localhost'
 define('DB_PORT', '3306');       // Puerto configurado en MAMP
-define('DB_USERNAME', 'root');  // Usuario predeterminado
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'touchIn'); // Nombre de la base de datos
+define('DB_USERNAME', 'u952930778_miguel');  // Usuario predeterminado
+define('DB_PASSWORD', 'Complex99@/');
+define('DB_DATABASE', 'u952930778_touchIn'); // Nombre de la base de datos
 // Zona horario
 date_default_timezone_set('America/Mexico_City');
